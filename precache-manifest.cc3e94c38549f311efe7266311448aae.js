b23ad6820369a844cd101a1dc90bcb290597abc5
Shakib448 <shakiba448@gmail.com>
2020-08-25 17:42:52 +0600

    Updates

---
+++ precache-manifest.cc3e94c38549f311efe7266311448aae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "732756db6aff294a32e068348620ca3d",
+    "url": "/ultra-nation/index.html"
+  },
+  {
+    "revision": "2571c09b38b4c376e2e7",
+    "url": "/ultra-nation/static/css/main.c604994d.chunk.css"
+  },
+  {
+    "revision": "7fda3fbd6cf7a66b4c2f",
+    "url": "/ultra-nation/static/js/2.121fbdbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ultra-nation/static/js/2.121fbdbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2571c09b38b4c376e2e7",
+    "url": "/ultra-nation/static/js/main.06b1db2a.chunk.js"
+  },
+  {
+    "revision": "dd6a5e3e6d8d7b323d8e",
+    "url": "/ultra-nation/static/js/runtime-main.8844c449.js"
+  }
+]);
