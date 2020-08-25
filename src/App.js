067ad7d45068818a50031f2b33c4dcbf65568209
Shakib448 <shakiba448@gmail.com>
2020-08-25 17:40:43 +0600

    React hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Country from './Components/Country/Country';
+import AddCountry from './Components/AddedCountry/AddCountry';
 
 function App() {
+
+  const [countries, setCountries] = useState([]);
+
+  const [addCountry, setAddCountry] = useState([]);
+
+  useEffect(() => {
+    // fetch('https://restcountries.eu/rest/v2/all')
+    // .then(res => res.json())
+    // .then(data => {
+    //   setCountries(data);
+    //   const name = data.map((country) => country.name).slice(0,10);
+    //   console.log(name);
+    // })
+    const loadData = async () => {
+      try {
+        const res = await fetch('https://restcountries.eu/rest/v2/all');
+        const data = await res.json();
+        setCountries(data);
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    loadData();
+
+  },[])
+
+  const hadleAddContry = (country) => {
+    const newSelectCountry = [...addCountry, country];
+    setAddCountry(newSelectCountry);
+  } // always pass peratmer
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+            <h1>Total country : {countries.length} </h1>
+          <AddCountry addCountry={addCountry}></AddCountry>
+
+        {
+          countries.map((country) => <Country country={country} key={country.alpha3Code} hadleAddContry={hadleAddContry}></Country>)
+        }
+      </div>
+  )
 }
 
 export default App;
